e5be0315d1d5fbd94e3866971f8522bc5b2f1c15
Juan Sebastian <jsfc2199@gmail.com>
2022-06-17 13:51:00 -0500

    Se realiza el fetch de la api de datros de coronavirus de Estadps Unidos

--- react-app/src/root.component.tsx
+++ react-app/src/root.component.tsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from "react";
+
 export default function Root(props) {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.covidtracking.com/v1/us/daily.json`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+
+      })
+  }, []);
+
   return (
-    <div>
+    <div className="App">
       <br></br>
-      <h1>Lo que quiera</h1>
+      <br></br>
+      <h1>Daily US Covid Data</h1>
+
+      <table>
+        <thead>
+          <tr>
+          <td>Date</td>
+            <td>Positive</td>
+            <td>Negative</td>
+            <td>Death</td>
+            <td>Number of States</td>
+          </tr>
+        </thead>
+        {data &&
+          data.map((data) => (
+            <tbody key={data.hash}>
+              <tr>
+              <td>{data.dateChecked}</td>
+              <td>{data.positive}</td>
+              <td>{data.negative}</td>
+              <td>{data.death}</td>
+              <td>{data.states}</td>
+              </tr>
+            </tbody>
+            
+          ))}
+  </table>
     </div>
   );
 }
